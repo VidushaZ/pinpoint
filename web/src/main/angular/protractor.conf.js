9258e430a7b3d09f0154f5a71a006f732ac9e49b
Vidusha-Lap <Vidushazoro@gmail.com>
2020-05-25 20:08:47 +0530

    Reset all

--- web/src/main/angular/protractor.conf.js
+++ web/src/main/angular/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://192.168.153.182:4200/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
